ba59a6d45fe1bf01d0d75bfdd03e95110a4be034
Volha Zholudzeva <lyovd@mail.ru>
2023-01-14 12:20:15 +0300

    Created classes: Address and Person

---
+++ SolutionLearnProject/Task1_Candidate/Classes/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Task1_Candidate.Classes
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+        public Person(string name, string lastName, Address address)
+        {
+            Name = name;
+            LastName = lastName;
+            Address = address;
+        }
+    }
+}
