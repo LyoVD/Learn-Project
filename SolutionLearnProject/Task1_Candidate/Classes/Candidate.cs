2b84523547bfef87a5f1b5b2ad540301e100871d
Volha Zholudzeva <lyovd@mail.ru>
2023-01-14 14:21:56 +0300

    Created classes: SubjectStore and Candidate

---
+++ SolutionLearnProject/Task1_Candidate/Classes/Candidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1_Candidate.Classes
+{
+    public class Candidate
+    {
+        public SubjectScore[] SubjectScore { get; set; }
+        public Person Person { get; set; }
+        public Candidate(Person person, SubjectScore[] subjectScore)
+        {
+            Person = person;
+            SubjectScore = subjectScore;
+
+        }
+    }
+}
